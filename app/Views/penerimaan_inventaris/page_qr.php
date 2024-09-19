71e9e10d3510f45b8c050306f79e4a023b3af955
bagusprasetyoo <bagusprstyoo11@gmail.com>
2024-09-20 03:59:39 +0800

    mengubah tombol cetak

--- app/Views/penerimaan_inventaris/page_qr.php
+++ app/Views/penerimaan_inventaris/page_qr.php
@@ -12,26 +12,27 @@
                     <div class="card-header">
                         <!-- <h3 class="card-title">Card with action</h3> -->
                         <div class="card-actions">
-                            <button type="button" class="btn btn-green" data-bs-toggle="modal" data-bs-target="#add_penerimaan_inventaris">
+                            <button type="button" class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#add_penerimaan_inventaris">
                                 <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M12 5l0 14"></path>
-                                    <path d="M5 12l14 0"></path>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-printer">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" />
+                                    <path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" />
+                                    <path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" />
                                 </svg>
-                                Tambah
+                                Cetak
                             </button>
                         </div>
                     </div>
                     <div class="card-body">
                         <h2>QR Codes</h2>
-                        <?php if (!empty($qrImages)): ?>
-                            <?php foreach ($qrImages as $image): ?>
+                        <?php if (!empty($qrImages)) : ?>
+                            <?php foreach ($qrImages as $image) : ?>
                                 <div style="margin-bottom: 20px;">
                                     <img src="<?= base_url('../uploads/' . basename($image)); ?>" alt="QR Code">
                                 </div>
                             <?php endforeach; ?>
-                        <?php else: ?>
+                        <?php else : ?>
                             <p>Tidak ada QR Code yang dihasilkan.</p>
                         <?php endif; ?>
                     </div>
