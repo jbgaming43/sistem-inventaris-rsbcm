50b92e3decc5e5635aff86ac75b576144ef2dbaa
jbgaming43 <jbhanjaya@gmail.com>
2024-10-09 08:53:22 +0700

    awal buat pembelian non medis

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -54,7 +54,7 @@ class Filters extends BaseConfig
             'filterAdmin' => [
                 'except' => [
                     'dashboard/*', 'dashboard', 'pengguna', 'pengguna/*', 
-                    'pengajuan_non_medis','pengajuan_non_medis/*','pengajuan_inventaris','pengajuan_inventaris/*',
+                    'pengajuan_non_medis','pengajuan_non_medis/*','pembelian_non_medis','pembelian_non_medis/*','pengajuan_inventaris','pengajuan_inventaris/*',
                     'pembelian_inventaris','pembelian_inventaris/*','penerimaan_inventaris','penerimaan_inventaris/*', 'pembayaran_inventaris',
                     'pembayaran_inventaris/*'
                 ]
