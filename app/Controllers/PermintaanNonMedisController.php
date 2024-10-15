b2e09fc3a9adac098c3edcb122b4c58e901adb4b
bagusprasetyoo <bagusprstyoo11@gmail.com>
2024-10-15 15:20:31 +0800

    merge

--- app/Controllers/PermintaanNonMedisController.php
+++ app/Controllers/PermintaanNonMedisController.php
@@ -3,80 +3,170 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
-use App\Models\PermintaanModel;
+use App\Models\PermintaanBarangNonMedisModel;
+use App\Models\PengajuanBarangNonMedisDetailModel;
+use App\Models\PegawaiModel;
+use App\Models\IpsrsBarangModel;
 use App\Helpers\AuthHelper;
 
-class PermintaanController extends BaseController
+class PermintaanNonMedisController extends BaseController
 {
     public function index()
     {
-        // objek PermintaanModel
-        $pengguna_mod = new PermintaanModel();
+        // objek PenggunaModel
+        $per_barang__nonmedis_mod = new PermintaanBarangNonMedisModel();
+        $pgwm = new PegawaiModel();
+        $ipsrsbarang_mod = new IpsrsBarangModel();
 
         $data = [
-            'title' => 'Data Pengguna',
-            'active_menu' => 'master',
-            'active_submenu' => 'pengguna',
+            'title' => 'Data Permintaan Non Medis',
+            'active_menu' => 'non_medis',
+            'active_submenu' => 'pengajuan_nonmedis',
 
-            'pengguna_con' => $pengguna_mod->getData()
+            'pbnc' => $per_barang__nonmedis_mod->getData(),
+            'pgwc' => $pgwm->getData(),
+            'ipsrsbarang_con' => $ipsrsbarang_mod->getData()
         ];
 
-        return view('pengguna/index', $data);
+
+        return view('pengajuan_nonmedis/index', $data);
     }
 
     public function add()
     {
-        // objek PermintaanModel
-        $pengguna_mod = new PermintaanModel();
+        // objek PenggunaModel
+        $pen_nonmedis_mod = new PengajuanBarangNonMedisModel();
+        $pen_nonmedis_det_mod = new PengajuanBarangNonMedisDetailModel();
 
-        // Upload file
-        $profile_image = $this->request->getFile('profile_image');
+        $no_pengajuan = $this->request->getPost('no_pengajuan');
+        $tanggal = $this->request->getPost('tanggal');
+        $nik = $this->request->getPost('nik');
+        $keterangan = $this->request->getPost('keterangan');
 
-        if ($profile_image && $profile_image->isValid() && !$profile_image->hasMoved()) {
-            // Mendapatkan ekstensi file
-            $ext = $profile_image->getClientExtension();
+        // Ambil data tabel barang yang diinputkan dalam bentuk array
+        $kode_brng = $this->request->getPost('kode_brng');
+        $kode_sat = $this->request->getPost('kode_sat');
+        $jumlah = $this->request->getPost('jumlah');
+        $h_pengajuan = $this->request->getPost('harga');
 
-            $newName = $this->request->getPost('username') . '.' . $ext;
-            $profile_image->move(ROOTPATH . 'public/assets/avatars', $newName);
-        } else {
-            $newName = 'default.jpg';
+        // Cek apakah semua input adalah array
+        if (!is_array($kode_brng) || !is_array($kode_sat) || !is_array($jumlah) || !is_array($h_pengajuan)) {
+            return redirect()->back()->with('error', 'Data input tidak valid.');
+        }
+
+        // Cek apakah panjang semua array sama
+        $arrayCount = min(count($kode_brng), count($kode_sat), count($jumlah), count($h_pengajuan));
+        if ($arrayCount === 0) {
+            return redirect()->back()->with('error', 'Tidak ada data yang valid untuk disimpan.');
         }
 
         $data = [
-            'username' => $this->request->getPost('username'),
-            'nama_pengguna' => $this->request->getPost('nama_pengguna'),
-            'level' => $this->request->getPost('level'),
-            'no_telp' => $this->request->getPost('no_telp'),
-            'profile_image' => $newName,
-            'created_at' => date('Y-m-d H:i:s'),
+            'no_pengajuan' => $no_pengajuan,
+            'nip' => $nik,
+            'tanggal' => $tanggal,
+            'status' => 'Proses Pengajuan',
+            'keterangan' => $keterangan,
         ];
 
-        $pengguna_mod->insertData($data);
+        $pen_nonmedis_mod->insertData($data);
+        $dataDetails = [];
+
+        for ($i = 0; $i < count($kode_brng); $i++) {
+            $dataDetails[] = [
+                'no_pengajuan' => $no_pengajuan,
+                'kode_brng' => $kode_brng[$i],
+                'kode_sat' => $kode_sat[$i],
+                'jumlah' => $jumlah[$i],
+                'h_pengajuan' => $h_pengajuan[$i],
+                'total' => $jumlah[$i] * $h_pengajuan[$i],
+            ];
+        }
+
+        // Insert semua data secara batch
+        $pen_nonmedis_det_mod->insertBatch($dataDetails);
 
         session()->setFlashdata('success', 'ditambahkan');
-        return redirect()->to('/pengguna');
+        return redirect()->to('/pengajuan_non_medis');
     }
 
-    public function edit($id)
+    public function detail($id)
     {
-        $pengguna_mod = new PermintaanModel();
+        $pen_nonmedis_det_mod = new PengajuanBarangNonMedisDetailModel();
+        $detail = $pen_nonmedis_det_mod->detailData($id);
 
-        $pengguna_mod->updateData($id, $data);
+        // Debug output
+        log_message('debug', 'Detail fetched: ' . json_encode($detail)); // Log detail untuk debug
 
-        session()->setFlashdata('success', 'diedit');
-        return redirect()->to('/pengguna');
+        if (empty($detail)) {
+            return $this->response->setJSON(['error' => 'Data tidak ditemukan']);
+        }
+
+        return $this->response->setJSON($detail);
     }
 
     public function delete($id)
     {
-        $pengguna_mod = new PermintaanModel();
+        $pen_nonmedis_mod = new PengajuanBarangNonMedisModel();
 
-        // Dapatkan data pengguna yang akan dihapus
-        $pengguna = $pengguna_mod->find($id);
-
-        $pengguna_mod->deleteData($id);
+        $pen_nonmedis_mod->deleteData($id);
 
         session()->setFlashdata('success', 'dihapus');
-        return redirect()->to('/pengguna');
+        return redirect()->to('/pengajuan_non_medis');
+    }
+
+    public function getBarangDetails()
+    {
+        $ipsrsbarang_mod = new IpsrsBarangModel();
+        $kode_barang = $this->request->getGet('kode_brng'); // Mengambil kode barang dari parameter GET
+
+        if ($kode_barang) {
+            $barang = $ipsrsbarang_mod->getDataByKode($kode_barang);
+            if ($barang) {
+                return $this->response->setJSON($barang);
+            }
+        }
+
+        return $this->response->setJSON(null); // Kembalikan JSON null jika barang tidak ditemukan
+    }
+
+    public function setuju($id)
+    {
+        $pen_nonmedis_mod = new PengajuanBarangNonMedisModel();
+
+        $data = [
+            'status' => 'Disetujui'
+        ];
+
+        $pen_nonmedis_mod->updateData($id, $data);
+
+        session()->setFlashdata('success', 'disetujui');
+        return redirect()->to('/pengajuan_non_medis');
+    }
+
+    public function tolak($id)
+    {
+        $pen_nonmedis_mod = new PengajuanBarangNonMedisModel();
+
+        $data = [
+            'status' => 'Ditolak'
+        ];
+
+        $pen_nonmedis_mod->updateData($id, $data);
+
+        session()->setFlashdata('success', 'ditolak');
+        return redirect()->to('/pengajuan_non_medis');
+    }
+
+    public function print($id)
+    {
+        $pen_nonmedis_mod = new PengajuanBarangNonMedisModel();
+        $pen_nonmedis_det_mod = new PengajuanBarangNonMedisDetailModel();
+
+        $data = [
+            'pen_nonmedis_con' => $pen_nonmedis_mod->getDataById($id),
+            'pen_nonmedis_det_con' => $pen_nonmedis_det_mod->detailData($id),
+        ];
+
+        return view('pengajuan_nonmedis/page_print', $data);
     }
 }
