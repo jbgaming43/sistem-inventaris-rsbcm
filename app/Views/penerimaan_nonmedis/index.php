80d5d173f39853909f6c1a691c9d1c3a9a25e5bf
bagusprasetyoo <bagusprstyoo11@gmail.com>
2024-10-15 09:46:40 +0800

    menghapus fitur garansi, ruang, qr code dalam penerimaan non medis

--- app/Views/penerimaan_nonmedis/index.php
+++ app/Views/penerimaan_nonmedis/index.php
@@ -12,24 +12,6 @@
                     <div class="card-header">
                         <!-- <h3 class="card-title">Card with action</h3> -->
                         <div class="card-actions">
-                            <button type="button" class="btn btn-red" data-bs-toggle="modal" data-bs-target="#add_garansi_penerimaan_nonmedis">
-                                <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M12 5l0 14"></path>
-                                    <path d="M5 12l14 0"></path>
-                                </svg>
-                                Garansi
-                            </button>
-                            <button type="button" class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#add_ruang_penerimaan_nonmedis">
-                                <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M12 5l0 14"></path>
-                                    <path d="M5 12l14 0"></path>
-                                </svg>
-                                Ruang
-                            </button>
                             <button type="button" class="btn btn-green" data-bs-toggle="modal" data-bs-target="#add_penerimaan_nonmedis">
                                 <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -74,25 +56,6 @@
                                                         <path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" />
                                                         <path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" />
                                                         <path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" />
-                                                    </svg>
-                                                </button>
-                                            </a>
-                                            <a href="<?= base_url('penerimaan_nonmedis/page_qr/' . $dt_penerimaan_nonmedis['no_faktur']) ?>">
-                                                <button type="button" class="btn btn-icon btn-dark btn-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="QR">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-qrcode">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                        <path d="M4 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
-                                                        <path d="M7 17l0 .01" />
-                                                        <path d="M14 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
-                                                        <path d="M7 7l0 .01" />
-                                                        <path d="M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
-                                                        <path d="M17 7l0 .01" />
-                                                        <path d="M14 14l3 0" />
-                                                        <path d="M20 14l0 .01" />
-                                                        <path d="M14 14l0 3" />
-                                                        <path d="M14 20l3 0" />
-                                                        <path d="M17 17l3 0" />
-                                                        <path d="M20 17l0 3" />
                                                     </svg>
                                                 </button>
                                             </a>
